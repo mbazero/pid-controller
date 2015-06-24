c4c9be645f112d9dc98a5d24a4939dda25a7a5ff
Michael Adams <mba13@duke.edu>
2015-06-24 16:50:34 -0400

    remove output_active from router and use negative routes to accomplish the same purpose.

--- router.v
+++ router.v
@@ -3,13 +3,14 @@
 // router -- mba 2014
 // -----------------------------------------------------------
 // Routes data streams from ADC input channels to DAC/DDS
-// output channels.
+// output channels. Negative source routes are invalid and
+// effectively deactivate the channel.
 // -----------------------------------------------------------
 
 module router #(
 	// parameters
 	parameter W_CHAN	= 16,										// width of input channels
-	parameter W_SEL	= 4,										// width of select signals
+	parameter W_SEL	= 5,										// width of select signals
 	parameter N_IN		= 8,										// number of input channels
 	parameter N_OUT	= 8,										// number of output channels
 	parameter ACTV_INIT = 1										// initial output activation
@@ -20,7 +21,6 @@ module router #(
 	// inputs <- frontpanel controller
 	input wire	[W_SEL-1:0]				src_select_in,		// source channel select
 	input wire	[W_SEL-1:0]				dest_select_in,	// destination channel select
-	input wire	[N_OUT-1:0]				output_active_in,	// output channel activation signals
 	input wire								update_in,			// update frontpanel params
 
 	// outputs -> output preprocessor
@@ -54,7 +54,7 @@ endgenerate
 genvar j;
 generate
 	for ( j = 0; j < N_OUT; j = j+1 ) begin : src_select_init
-		initial src_select[j] = 0;
+		initial src_select[j] = -1;
 	end
 endgenerate
 
@@ -78,7 +78,7 @@ generate
 		mux_inst (
 			.data_packed_in	(data_packed_in),
 			.chan_select_in	(src_select[k]),
-			.enable_in			(output_active_in[k]),
+			.enable_in			(~src_select[k][W_SEL-1]), // negative routes disable output
 			.data_out			(mux_data_out[k])
 			);
 		assign data_out[k] = mux_data_out[k];
