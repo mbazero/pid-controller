038ed6ae3b98e0bd4fd4c87a219e966b236ae31c
Michael Adams <mba13@duke.edu>
2015-07-08 17:23:04 -0400

    change instr dispatch to maintain source channel map.

--- instr_dispatch.v
+++ instr_dispatch.v
@@ -50,7 +50,7 @@ localparam NULL_SRC = {1'b1, {W_SRC{1'b0}}};
 localparam NULL_CHAN = {1'b1, {W_CHAN{1'b0}}};
 
 //--------------------------------------------------------------------
-// Configuration Memory
+// External Memory
 //--------------------------------------------------------------------
 reg [N_CHAN-1:0] chan_en_mem;
 reg [W_SRC:0] chan_src_sel_mem[0:N_CHAN-1];
@@ -58,91 +58,112 @@ reg [W_SRC:0] chan_src_sel_mem[0:N_CHAN-1];
 // Initialize
 integer i;
 initial begin
-    for ( i = 0; i < N_CHAN; i = i+1 ) begin
+    for ( i = 0; i < N_CHAN; i = i + 1 ) begin
         chan_en_mem[i] = 0;
         chan_src_sel_mem[i] = NULL_SRC;
     end
 end
 
 // Handle write requests
+wire wr_chan_valid = ( wr_chan < N_CHAN );
 always @( posedge clk_in ) begin
-    if ( wr_en ) begin
+    if ( wr_en && wr_chan_valid ) begin
         case ( wr_addr )
-            chan_src_sel_addr : chan_src_sel_mem[wr_chan] <= wr_data[W_SRC:0];
             chan_en_addr : chan_en_mem[wr_chan] <= wr_data[0];
+            chan_src_sel_addr : chan_src_sel_mem[wr_chan] <= wr_data[W_SRC:0];
         endcase
     end
 end
 
 //--------------------------------------------------------------------
-// Data Buffer
+// Source Channel Map
 //--------------------------------------------------------------------
-wire [W_SRC-1:0] fifo_src;
-wire [W_DATA-1:0] fifo_data;
-wire fifo_dv;
-reg fifo_rd_en;
+reg [N_CHAN-1:0] src_chan_map[0:W_SRC-1];
+
+// Initialize
+initial begin
+    for (i = 0; i < N_SRC; i = i + 1 ) begin
+        src_chan_map[i] = 0;
+    end
+end
+
+// Update chan map when new route is received
+always @( posedge clk_in ) begin
+    if ( wr_en && wr_chan_valid
+        && ( wr_addr == chan_src_sel_addr )) begin
+        // Clear old mapping
+        src_chan_map[chan_src_sel_mem[wr_chan]] <= 0;
+
+        // Register new mapping if valid
+        if ( wr_data[W_SRC-1:0] < N_SRC ) begin
+            src_chan_map[wr_data[W_SRC-1:0]] <= 1;
+        end
+    end
+end
+
+//--------------------------------------------------------------------
+// Input Buffer
+//--------------------------------------------------------------------
+wire [W_SRC-1:0] buf_src;
+wire [W_DATA-1:0] buf_data;
+wire buf_dv;
+reg buf_rd_en;
 
 fifo_21 input_buffer (
    .clk     (clk_in),
    .rst     (rst_in),
    .din     ({src_in, data_in}),
    .wr_en   (dv_in),
-   .rd_en   (fifo_rd_en),
-   .dout    ({fifo_src, fifo_data}),
-   .valid   (fifo_dv),
+   .rd_en   (buf_rd_en),
+   .dout    ({buf_src, buf_data}),
+   .valid   (buf_dv),
    .full    (),
    .empty   ()
    );
 
 //--------------------------------------------------------------------
-// Channel Decoder
+// Dispatcher
 //--------------------------------------------------------------------
-reg dec_dv;
-reg [W_CHAN:0] dec_chan;
+reg dspch_dv;
+reg [W_CHAN:0] dspch_chan;
 reg [N_CHAN-1:0] instr_sent;
 
-// Decode PID channel giving priority to lower numbered channels. If
-// valid channel found, inject instruction into pipeline and continue
-// search for other valid channels. If no other valid channels found,
-// pull new data from the FIFO. Observe that blocking assignment are
-// used. Decoder functionality depends on this.
+// Dispatch instructions if source has valid channel mappings and if
+// the mapped channels are activated. Otherwise, pull new data from
+// the input buffer. A new instruction is dispatched every clock cycle
+// for each valid channel mapping. Lower numbered channels are given
+// dispatch priority.
 always @( posedge clk_in ) begin
-    // Default null channel assignment if source is not routed
-    dec_chan = NULL_CHAN;
+    if ( rst_in | ~buf_dv ) begin
+        dspch_dv = 0;
+        instr_sent = 0;
+        buf_rd_en = 0;
 
-    // Decode PID channel
-    for ( i = N_CHAN - 1; i >= 0; i = i - 1 ) begin
-        if ( chan_src_sel_mem[i] == fifo_src
-            && !instr_sent[i] ) begin
-            dec_chan = i;
+    end else if ( |src_chan_map[buf_src] ) begin
+        // Decode channel number
+        for ( i = N_CHAN - 1; i >= 0; i = i - 1 ) begin
+            if ( src_chan_map[i] && !instr_sent[i] ) begin
+                dspch_chan = i;
+            end
         end
-    end
 
-    // Clear registers if reset is high or fifo data is invalid.
-    // Otherwise, if no valid PID channel found, clear sent
-    // instruction map and pull new data from input buffer.
-    // Otherwise, channel is valid, so send instruction
-    // if the channel is active and set sent flag.
-    if ( rst_in | ~fifo_dv ) begin
-        dec_dv = 0;
-        instr_sent = 0;
-        fifo_rd_en = 0;
-    end else if ( dec_chan >= N_CHAN ) begin
-        dec_dv = 0;
-        instr_sent = 0;
-        fifo_rd_en = 1;
+        // Dispatch instruction
+        dspch_dv = chan_en_mem[dspch_chan];
+        instr_sent[dspch_chan] = 1;
+        buf_rd_en = 0;
+
     end else begin
-        dec_dv = chan_en_mem[dec_chan];
-        instr_sent[dec_chan] = 1;
-        fifo_rd_en = 0;
+        dspch_dv = 0;
+        instr_sent = 0;
+        buf_rd_en = 1;
     end
 end
 
 //--------------------------------------------------------------------
 // Output Assignment
 //--------------------------------------------------------------------
-assign dv_out = dec_dv;
-assign chan_out = dec_chan[W_CHAN-1:0];
-assign data_out = fifo_data;
+assign dv_out = dspch_dv;
+assign chan_out = dspch_chan[W_CHAN-1:0];
+assign data_out = buf_data;
 
 endmodule
